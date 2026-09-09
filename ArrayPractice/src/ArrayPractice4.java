9355e7dd5d0e15795301490de8efca5247bc5970
Stella Preinitz Gärdinge <318149449+stellapreinitz@users.noreply.github.com>
2026-09-09 17:23:08 +0200

    ArrayPractice4 done

---
+++ ArrayPractice/src/ArrayPractice4.java
@@ -0,0 +1,26 @@
+public class ArrayPractice4
+{
+    static void main(String[] args)
+    {
+        int[][] grid = new int[5][5];
+        int gridSum = 0;
+
+        for (int indexX = 0; indexX < 5; indexX++)
+        {
+            for (int indexY = 0; indexY < 5; indexY++)
+            {
+                grid[indexX][indexY] = indexX + indexY;
+                gridSum += grid[indexX][indexY];
+            }
+        }
+        for (int indexX = 0; indexX < 5; indexX++)
+        {
+            for (int indexY = 0; indexY < 5; indexY++)
+            {
+                System.out.print(grid[indexX][indexY] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nGrid Sum: " + gridSum);
+    }
+}
