cd78280ef6cd9245af5fb8055c7f26cef145d21b
stellapreinitz <318149449+stellapreinitz@users.noreply.github.com>
2026-09-10 10:34:31 +0200

    Second pass, array size move to field declaration, borderSum if clause corrected

--- ArrayPractice/src/ArrayPractice4.java
+++ ArrayPractice/src/ArrayPractice4.java
@@ -2,25 +2,28 @@ public class ArrayPractice4
 {
     static void main(String[] args)
     {
-        int[][] grid = new int[5][5];
+        int gridSizeX = 5;
+        int gridSizeY = 5;
+        int[][] grid = new int[gridSizeX][gridSizeY];
         int gridSum = 0;
         int borderSum = 0;
 
-        for (int indexX = 0; indexX < 5; indexX++)
+        for (int indexX = 0; indexX < gridSizeX; indexX++)
         {
-            for (int indexY = 0; indexY < 5; indexY++)
+            for (int indexY = 0; indexY < gridSizeY; indexY++)
             {
                 grid[indexX][indexY] = indexX + indexY;
                 gridSum += grid[indexX][indexY];
-                if (indexX == 0 || indexY == 0 || indexX == (grid.length - 1) || indexY == (grid.length - 1))
+                //gridSize-1 used because array index starts at 0, will always be 1 less than gridSize
+                if (indexX == 0 || indexY == 0 || indexX == gridSizeX-1 || indexY == gridSizeY-1)
                 {
                     borderSum += grid[indexX][indexY];
                 }
             }
         }
-        for (int indexX = 0; indexX < 5; indexX++)
+        for (int indexX = 0; indexX < gridSizeX; indexX++)
         {
-            for (int indexY = 0; indexY < 5; indexY++)
+            for (int indexY = 0; indexY < gridSizeY; indexY++)
             {
                 System.out.print(grid[indexX][indexY] + " ");
             }
