a8357e9c254ed8187538d449a456cb9ab1a41732
stellapreinitz <318149449+stellapreinitz@users.noreply.github.com>
2026-09-13 18:08:12 +0200

    main expanded, askQuestions renamed to quiz, method declaratins added: showResult, evaluateScore, askQuestion

--- InteractiveQuiz/src/Quiz.java
+++ InteractiveQuiz/src/Quiz.java
@@ -7,11 +7,11 @@ public class Quiz
     static void main(String[] args)
     {
         int score = 0;
-        String grade;
 
         presentGame();
-        askQuestions();
-        //showResult(score, grade = evaluateScore(score));
+        score += quiz();
+        char grade = (char) evaluateScore(score);
+        showResult(score, grade);
     }
     public static void presentGame()
     {
@@ -19,21 +19,75 @@ public class Quiz
         printDivider();
         System.out.println("The quiz contains 5 questions");
         System.out.println("each with 4 answering options.");
+        System.out.println("Type the corresponding number to choose option.");
         System.out.println("Score will be kept, you will be graded.");
         System.out.println("Good luck!");
         printDivider();
-        System.out.println("Press any key to begin");
+        System.out.println("Press Enter to begin");
         scanner.nextLine();
     }
     public static void printDivider()
     {
         System.out.println("====================");
     }
-    public static void askQuestions()
+    public static int quiz()
     {
+        int score = 0;
         for (int index = 0; index < 5; index++)
         {
-            System.out.println("Question number " + index + ":");
+            System.out.println("Question number " + (index+1) + ":");
+            score += askQuestion("q", new String[4],3 );
+        }
+        return score;
+    }
+    public static void showResult(int score, char grade)
+    {
+        printDivider();
+        System.out.println("Score: " + score + "\nGrade: " + grade);
+    }
+    public static double evaluateScore(int score)
+    {
+        switch (score)
+        {
+            case 0 -> {return 'F';}
+            case 1 -> {return 'E';}
+            case 2 -> {return 'D';}
+            case 3 -> {return 'C';}
+            case 4 -> {return 'B';}
+            case 5 -> {return 'A';}
+            default -> {return '-';}
+        }
+    }
+    public static int askQuestion(String questionText, String[] answerOptions, int correctOption)
+    {
+        int answer;
+        System.out.println(questionText);
+        for (int index = 0; index < answerOptions.length; index++)
+        {
+            System.out.println(answerOptions[index]);
+        }
+        while (true)
+        {
+            try
+            {
+                answer = Integer.parseInt(scanner.nextLine());
+                if (answer == correctOption)
+                {
+                    return 1;
+                }
+                else if (answer <= answerOptions.length && answer > 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    System.out.println("Not a valid answer, try again.");
+                }
+            }
+            catch (NumberFormatException error)
+            {
+                System.out.println("Not a valid answer, try again.");
+            }
         }
     }
 }
