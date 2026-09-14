c717c37583c61de7a12e910ba64cda65e24998a2
stellapreinitz <318149449+stellapreinitz@users.noreply.github.com>
2026-09-14 15:01:25 +0200

    Quiz done, quiz() now contains question data and parses it to askQuestion() through a new class QuizQuestion

--- InteractiveQuiz/src/Quiz.java
+++ InteractiveQuiz/src/Quiz.java
@@ -32,14 +32,59 @@ public class Quiz
     }
     public static int quiz()
     {
-        String[] question = {"q1", "q2", "q3", "q4", "q5"};
-        String[][] answerOptions = {{"o1","o2","o3","o4"},{"o1","o2","o3","o4"},{"o1","o2","o3","o4"},{"o1","o2","o3","o4"},{"o1","o2","o3","o4"}};
-        int[] correctOptions = {3,4,1,3,4};
+        //when adding or removing questions, make sure to edit the assignments of questions[] AND numberOfQuestions in synch
         int score = 0;
-        for (int index = 0; index < question.length; index++)
+        int numberOfQuestions = 5;
+        QuizQuestion[] questions = new QuizQuestion[numberOfQuestions];
+        questions[0] = new QuizQuestion
+                ("What does the Java keyword char mean?",
+                new String[] {
+                        "1. It stores a single letter, number or symbol",
+                        "2. A pointer type that stores the memory address of a string array",
+                        "3. Short for \"characteristic value\", a data type used to hold the exponent part of a float",
+                        "4. It purges a variable from memory"},
+                1
+                );
+        questions[1] = new QuizQuestion
+                ("What does OOP mean in the context of programming?",
+                        new String[] {
+                                "1. Order of parsing",
+                                "2. Object ordered parameter",
+                                "3. Object oriented programming",
+                                "4. Orthodox object pointer"},
+                        3
+                );
+        questions[2] = new QuizQuestion
+                ("Which are the possible values of a boolean in Java?",
+                        new String[] {
+                                "1. 0 or 1",
+                                "2. true or false",
+                                "3. Any value between 0.0 and 1.0",
+                                "4. true, false or null"},
+                        2
+                );
+        questions[3] = new QuizQuestion
+                ("What is does the operator \"==\" do?",
+                        new String[] {
+                                "1. Compares any values to check in they are the same",
+                                "2. Assigns value to a variable",
+                                "3. Compares any primitive types to check if their values are the same",
+                                "4. Compares any compatible primitive types to check if their values are the same"},
+                        4
+                );
+        questions[4] = new QuizQuestion
+                ("Do all Java programs need a main method?",
+                        new String[] {
+                                "1. Yes, always",
+                                "2. No, never",
+                                "3. No, not if the program has another entry point",
+                                "4. Yes, except in Java 26"},
+                        3
+                );
+        for (int index = 0; index < questions.length; index++)
         {
             System.out.println("Question number " + (index+1) + ":");
-            score += askQuestion(question[index], answerOptions[index],3 );
+            score += askQuestion(questions[index]);
         }
         return score;
     }
@@ -61,25 +106,29 @@ public class Quiz
             default -> {return '-';}
         }
     }
-    public static int askQuestion(String questionText, String[] answerOptions, int correctOption)
+    public static int askQuestion(QuizQuestion question)
     {
         int answer;
-        System.out.println(questionText);
-        for (int index = 0; index < answerOptions.length; index++)
+        System.out.println(question.questionPrompt);
+        for (int index = 0; index < question.questionAlternatives.length; index++)
         {
-            System.out.println(answerOptions[index]);
+            System.out.println(question.questionAlternatives[index]);
         }
         while (true)
         {
             try
             {
                 answer = Integer.parseInt(scanner.nextLine());
-                if (answer == correctOption)
+                if (answer == question.questionSolution)
                 {
+                    System.out.println("Correct!");
+                    printDivider();
                     return 1;
                 }
-                else if (answer <= answerOptions.length && answer > 0)
+                else if (answer <= question.questionAlternatives.length && answer > 0)
                 {
+                    System.out.println("Incorrect.");
+                    printDivider();
                     return 0;
                 }
                 else
@@ -92,5 +141,18 @@ public class Quiz
                 System.out.println("Not a valid answer, try again.");
             }
         }
+    }
+}
+class QuizQuestion
+{
+    String questionPrompt;
+    String[] questionAlternatives;
+    int questionSolution;
+
+    public QuizQuestion(String questionPrompt, String[] questionAlternatives, int questionSolution)
+    {
+        this.questionPrompt = questionPrompt;
+        this.questionAlternatives = questionAlternatives;
+        this.questionSolution = questionSolution;
     }
 }
