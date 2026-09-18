6e88276a314de5c572ff36e47b676d00224e0b71
stellapreinitz <318149449+stellapreinitz@users.noreply.github.com>
2026-09-18 09:08:47 +0200

    Removed extra instantiation of Catalogue

--- SongRegistry/src/ConsoleUI.java
+++ SongRegistry/src/ConsoleUI.java
@@ -12,8 +12,6 @@ public class ConsoleUI
     private static final String BOLD = "\u001B[1m";
     private static final String BLUE = "\u001B[34m";
 
-    Catalogue catalogue = new Catalogue();
-
     public void printHeader()
     {
         printDivider();
